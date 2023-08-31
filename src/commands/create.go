4090bb325daa95eb9e6c6bc0f1f3726b4b44dda3
jv <jvitoroliveira0711@gmail.com>
2023-08-31 00:32:42 -0300

    feature: Implementa validação para garantir que o usuário vai informar o conteúdo da nota

--- src/commands/create.go
+++ src/commands/create.go
@@ -3,13 +3,18 @@ package commands
 import (
 	"fmt"
 	"github.com/spf13/cobra"
+	"os"
+	"quick_notes/src/utils"
 )
 
 var create = &cobra.Command{
 	Use:   "create",
 	Short: "Comando responsável por cadastrar uma nova nota",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Hello, World")
+		if len(args) == 0 {
+			fmt.Println(utils.GetErrorColor("Por favor, informe o conteúdo da nota."))
+			os.Exit(1)
+		}
 	},
 }
 
