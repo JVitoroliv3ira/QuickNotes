716e2fd471887aa63ef9bdf9b6356a0b788091d2
jv <jvitoroliveira0711@gmail.com>
2023-08-31 13:16:03 -0300

    feature: Utiliza o método create do note_service

--- src/commands/create.go
+++ src/commands/create.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"github.com/spf13/cobra"
 	"os"
+	"quick_notes/src/services"
+	"quick_notes/src/types"
 	"quick_notes/src/utils"
 )
 
@@ -13,6 +15,11 @@ var create = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 		if len(args) == 0 {
 			fmt.Println(utils.GetErrorColor("Por favor, informe o conteúdo da nota."))
+			os.Exit(1)
+		}
+		_, err := services.Create(types.Note{Text: args[0]})
+		if err != nil {
+			fmt.Println(utils.GetErrorColor("Ocorreu um erro inesperado durante a criação da nota."))
 			os.Exit(1)
 		}
 		fmt.Println(utils.GetSuccessColor("Nota cadastrada com sucesso!"))
