600128f725dbb0ac9a213a1a8fef824af4134852
jv <jvitoroliveira0711@gmail.com>
2023-08-31 10:37:56 -0300

    feature: Adiciona função responsável por criar a cor utilizada nas mensagens de sucesso

--- src/utils/color_utils.go
+++ src/utils/color_utils.go
@@ -5,3 +5,7 @@ import "github.com/fatih/color"
 func GetErrorColor(message string) string {
 	return color.New(color.FgRed).SprintFunc()(message)
 }
+
+func GetSuccessColor(message string) string {
+	return color.New(color.FgGreen).SprintFunc()(message)
+}
