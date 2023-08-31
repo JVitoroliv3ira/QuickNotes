e92659f9b79dffe5e387834f739ea7bfb915b706
jv <jvitoroliveira0711@gmail.com>
2023-08-31 00:04:38 -0300

    feature: Cria estrutura do arquivo main

--- main.go
+++ main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+	"os"
+	"quick_notes/src/commands"
+)
 
+var rootCmd = &cobra.Command{
+	Use:   "",
+	Short: "Uma ferramenta para gerenciar notas rápidas.",
+}
+
+func main() {
+	rootCmd.AddCommand(commands.GetCreateCommand())
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
