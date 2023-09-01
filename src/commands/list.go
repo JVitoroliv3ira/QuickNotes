da93889c1b009eac6878c485f711c632f36daed1
jv <jvitoroliveira0711@gmail.com>
2023-09-01 12:14:31 -0300

    feature: Finaliza o comando de listar as notas

--- src/commands/list.go
+++ src/commands/list.go
@@ -3,13 +3,23 @@ package commands
 import (
 	"fmt"
 	"github.com/spf13/cobra"
+	"os"
+	"quick_notes/src/repositories"
+	"quick_notes/src/utils"
+	"quick_notes/src/views"
 )
 
 var list = &cobra.Command{
 	Use:   "ls",
 	Short: "Comando responsável por listar as notas cadastradas.",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Hello, World!")
+		list, err := repositories.FindAll()
+		if err != nil {
+			fmt.Println(utils.GetErrorColor("Ocorreu um erro inesperado durante a listagem das notas."))
+			os.Exit(1)
+		}
+
+		views.Render(list)
 	},
 }
 
