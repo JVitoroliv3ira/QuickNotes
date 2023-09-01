8ffd4f1db450f7ae045986e3fd7a5934ac50f900
jv <jvitoroliveira0711@gmail.com>
2023-09-01 14:56:30 -0300

    feature: Adiciona implementação inicial do comando de copiar

--- src/commands/copy.go
+++ src/commands/copy.go
@@ -2,14 +2,30 @@ package commands
 
 import (
 	"fmt"
+	"github.com/atotto/clipboard"
 	"github.com/spf13/cobra"
+	"os"
+	"quick_notes/src/repositories"
+	"quick_notes/src/utils"
 )
 
 var cp = &cobra.Command{
 	Use:   "cp",
 	Short: "Comando responsável por copiar uma nota para a área de transferência.",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Hello, World")
+		if len(args) == 0 {
+			fmt.Println(utils.GetErrorColor("Informe o identificador da nota que você deseja copiar."))
+			os.Exit(1)
+		}
+		note, err := repositories.FindById(args[0])
+		if err != nil {
+			panic(err)
+		}
+		err = clipboard.WriteAll(note.Text)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println(utils.GetSuccessColor("Nota copiada para a área de transferência com sucesso"))
 	},
 }
 
