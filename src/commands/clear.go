390f6c68aa4afe2194f4c45544cbb66ee203fd8b
jv <jvitoroliveira0711@gmail.com>
2023-09-02 20:56:44 -0300

    feature: Implementa comando responsavel por limpar as notas

--- src/commands/clear.go
+++ src/commands/clear.go
@@ -3,13 +3,21 @@ package commands
 import (
 	"fmt"
 	"github.com/spf13/cobra"
+	"os"
+	"quick_notes/src/repositories"
+	"quick_notes/src/utils"
 )
 
 var clear = &cobra.Command{
 	Use:   "clear",
 	Short: "Comando responsável por limpar todas as notas",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Hello, World!")
+		err := repositories.DeleteAll()
+		if err != nil {
+			fmt.Println(utils.GetErrorColor("Ocorreu um erro ao deletar as notas: " + err.Error()))
+			os.Exit(1)
+		}
+		fmt.Println(utils.GetSuccessColor("Notas deletadas com sucesso!"))
 	},
 }
 
