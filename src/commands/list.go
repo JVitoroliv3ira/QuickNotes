3ed74ed3d78a438aa61e51bca07ef0f61c181fd5
jv <jvitoroliveira0711@gmail.com>
2023-09-02 21:37:06 -0300

    feature: Utiliza o método do service

--- src/commands/list.go
+++ src/commands/list.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"github.com/spf13/cobra"
 	"os"
-	"quick_notes/src/repositories"
+	"quick_notes/src/services"
 	"quick_notes/src/utils"
 	"quick_notes/src/views"
 )
@@ -13,7 +13,7 @@ var list = &cobra.Command{
 	Use:   "ls",
 	Short: "Comando responsável por listar as notas cadastradas.",
 	Run: func(cmd *cobra.Command, args []string) {
-		list, err := repositories.FindAll()
+		list, err := services.ListAll()
 		if err != nil {
 			fmt.Println(utils.GetErrorColor("Ocorreu um erro inesperado durante a listagem das notas."))
 			os.Exit(1)
